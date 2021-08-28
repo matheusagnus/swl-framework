1d95c27132641c26b949d23bacb99792ca92b53e
Matheus Ferreira <matheusferreira@MacBook-Pro-de-Matheus.local>
2021-08-28 18:11:39 -0300

    :sparkles: setupping default layout

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,21 @@
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'Home',
+
+  mounted () {
+    var root = document.getElementsByTagName( 'html' )[0]
+
+    if (false) {
+      root.classList.add('partner-1')
+    } else {
+      root.classList.add('default')
+    }
+  }
+}
 </script>
+
+<style lang="scss">
+@import '@/assets/stylesheet/styleguide.scss'
+</style>
